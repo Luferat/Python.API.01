8ca950870f084759e67f7583f30821972501bcbe
Luferat <contato@luferat.net>
2023-11-08 20:22:31 -0300

    Ajustes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Python.API.01
  Minha primeira API REST em Python.
+
+## Requisitos
+
+- Python 3.10^
---
+++ api01.py
@@ -0,0 +1,18 @@
+items = [
+    {
+        "id": 1,
+        "name": "Bagulho",
+        "description": "Apenas um bagulho",
+        "location": "Em uma caixa"
+    }, {
+        "id": 2,
+        "name": "Tranqueira",
+        "description": "Apenas uma tranqueira qualquer",
+        "location": "Em um gaveteiro"
+    }, {
+        "id": 3,
+        "name": "Bagulete",
+        "description": "Um bagulete qualquer",
+        "location": "Na esquina"
+    }
+]
