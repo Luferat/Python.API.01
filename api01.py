95d431e202ea6b86b6d21762061656ad5b8c9318
Luferat <contato@luferat.net>
2023-11-09 17:57:13 -0300

    Update api01.py

--- api01.py
+++ api01.py
@@ -20,17 +20,25 @@ items = [
     }
 ]
 
+# Função que lê e lista todos os itens da coleção.
 def get_all():
-    # Converte o dicionario 'items' para json e armazena em 'var_json'
+    # Converte a lusta 'items' para json e armazena em 'var_json'
     var_json = json.dumps(items, indent=2)
     
     # Imprime o json.
     print(var_json)
 
+# Função que lê um item específico, identificado pelo índice.
 def get_one(id):
+    
+    # Converte o dicionario 'items[id]' para json e armazena em 'var_json'
     var_json = json.dumps(items[id], indent=2)
+    
+    # Imprime o json.
     print(var_json)
     
-# get_all()
+# Chama (call) a função get_all().
+get_all()
 
+# Chama a função get_one(), passando o índice como parâmetro.
 get_one(1)
